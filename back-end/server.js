f552dbce94f06a77effc85f3521520ce4aaa150d
billmcconnell <44976673+billmcconnell@users.noreply.github.com>
2023-12-07 00:01:07 -0500

    adding swagger docs

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const dal = require('./dal.js');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
