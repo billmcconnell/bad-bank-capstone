2f52ecf387b2a19da52ece22ee0e7ffd4061a8b9
billmcconnell <44976673+billmcconnell@users.noreply.github.com>
2023-12-09 17:04:15 -0500

    added path code to server.js

--- back-end/server.js
+++ back-end/server.js
@@ -149,6 +149,12 @@ app.get('/account/all', function (req, res) {
         });
 });
 
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
